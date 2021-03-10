8fb9f850926857e0231f09001484e0a0e46bb724
ordamari <ordamari971@gmail.com>
2021-03-10 13:17:03 +0200

    bdika

--- src/app.js
+++ src/app.js
@@ -5,22 +5,25 @@ import { IsUserRedirect, ProtectedRoute } from './helpers/routes'
 import { useAuthListener } from './hooks'
 
 export default function App() {
-  const {user} = useAuthListener();
+  const { user } = useAuthListener();
 
   return (
-    <Router>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact >
-        <Signin />
-      </IsUserRedirect>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact >
-        <Signup />
-      </IsUserRedirect>
-      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact >
-        <Home />
-      </IsUserRedirect>
-      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-        <Browse />
-      </ProtectedRoute>
-    </Router>
+    <>
+    <h1>hisajdniopasjmdmasd</h1>
+      <Router>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact >
+          <Signin />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact >
+          <Signup />
+        </IsUserRedirect>
+        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact >
+          <Home />
+        </IsUserRedirect>
+        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+          <Browse />
+        </ProtectedRoute>
+      </Router>
+    </>
   );
 }
