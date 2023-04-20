cc885d23d71bb803480c4ee99707d82ffe7f8f5a
ordamari <ordamari971@gmail.com>
2023-04-20 22:33:01 +0300

    Try fix deploy bug 2

--- src/app.js
+++ src/app.js
@@ -8,19 +8,19 @@ export default function App() {
   const { user } = useAuthListener();
 
   return (
-      <Router>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact >
-          <Signin />
-        </IsUserRedirect>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact >
-          <Signup />
-        </IsUserRedirect>
-        <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact >
-          <Home />
-        </IsUserRedirect>
-        <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
-          <Browse />
-        </ProtectedRoute>
-      </Router>
+    <Router basename={'/'} >
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_IN} exact >
+        <Signin />
+      </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.SIGN_UP} exact >
+        <Signup />
+      </IsUserRedirect>
+      <IsUserRedirect user={user} loggedInPath={ROUTES.BROWSE} path={ROUTES.HOME} exact >
+        <Home />
+      </IsUserRedirect>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE} exact>
+        <Browse />
+      </ProtectedRoute>
+    </Router>
   );
 }
